143924380e0adb76ba4a4a4c23e22f496f0fcd32
mijures <1339236493@qq.com>
2020-03-21 01:04:56 +0800

    V0.8 trot步态完成，可平移+旋转      当前运算量偏大，运算频率大概10Hz，需要优化      参数还没调完，机体重心不在正中央，需要加偏移量

--- Quadruped Robot/My_Drivers/Tasks/InitTask.c
+++ Quadruped Robot/My_Drivers/Tasks/InitTask.c
@@ -5,6 +5,7 @@
 #include "tim.h"
 #include "DBUS.h"
 #include "bsp_imu.h"
+#include "legctrl.h"
 
 
 osThreadId MainHandle;
@@ -15,41 +16,44 @@ void StartInitTask(void const * argument)
 {
 	HAL_TIM_PWM_Start(&htim12,TIM_CHANNEL_1);
 	
-	__HAL_TIM_SET_AUTORELOAD(&htim12,2000);
-	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,1000);
-	osDelay(100);
-	__HAL_TIM_SET_AUTORELOAD(&htim12,1500);
-	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,750);
-	osDelay(180);
-	__HAL_TIM_SET_AUTORELOAD(&htim12,500);
-	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,250);
-	osDelay(280);
-	__HAL_TIM_SET_AUTORELOAD(&htim12,500);
-	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,0);
+//	__HAL_TIM_SET_AUTORELOAD(&htim12,2000);
+//	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,1000);
+//	osDelay(100);
+//	__HAL_TIM_SET_AUTORELOAD(&htim12,1500);
+//	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,750);
+//	osDelay(180);
+//	__HAL_TIM_SET_AUTORELOAD(&htim12,500);
+//	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,250);
+//	osDelay(280);
+//	__HAL_TIM_SET_AUTORELOAD(&htim12,500);
+//	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,0);
 
 	DBUS_Init();
 	IMU_INIT();
+	robot_init(legs);
 	HAL_GPIO_WritePin(D1_GPIO_Port,D1_Pin|D2_Pin|D3_Pin|D4_Pin|D5_Pin|D6_Pin|D7_Pin|D8_Pin,GPIO_PIN_SET);
 	HAL_GPIO_WritePin(LED_R_GPIO_Port,LED_R_Pin,GPIO_PIN_SET);
 	HAL_GPIO_WritePin(LED_G_GPIO_Port,LED_G_Pin,GPIO_PIN_SET);
-		__HAL_TIM_SET_AUTORELOAD(&htim12,500);
-	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,250);
-	osDelay(180);
-		__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,0);
-		osDelay(180);
-		__HAL_TIM_SET_AUTORELOAD(&htim12,500);
-	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,250);
-	osDelay(180);
-	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,0);
-		osDelay(180);
-	osThreadDef(MainTask,StartMainTask,osPriorityHigh,0,512);
+	
+//		__HAL_TIM_SET_AUTORELOAD(&htim12,500);
+//	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,250);
+//	osDelay(180);
+//		__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,0);
+//		osDelay(180);
+//		__HAL_TIM_SET_AUTORELOAD(&htim12,500);
+//	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,250);
+//	osDelay(180);
+//	__HAL_TIM_SET_COMPARE(&htim12,TIM_CHANNEL_1,0);
+//		osDelay(180);
+		
+	osThreadDef(MainTask,StartMainTask,osPriorityHigh,0,2048);
 	MainHandle = osThreadCreate(osThread(MainTask),NULL);
 //	
 //	osThreadDef(MonitorTask,StartMonitorTask,osPriorityAboveNormal,0,1024);
 //	MonitorHandle = osThreadCreate(osThread(MonitorTask),NULL);
 //	
-//	osThreadDef(ShowTask,StartShowTask,osPriorityNormal,0,512);
-//	MonitorHandle = osThreadCreate(osThread(ShowTask),NULL);
+	osThreadDef(ShowTask,StartShowTask,osPriorityNormal,0,512);
+	MonitorHandle = osThreadCreate(osThread(ShowTask),NULL);
 	
 	osThreadTerminate(NULL);
 }
--- Quadruped Robot/My_Drivers/Tasks/MainTask.c
+++ Quadruped Robot/My_Drivers/Tasks/MainTask.c
@@ -3,56 +3,24 @@
 #include "tim.h"
 #include "DBUS.h"
 #include "bsp_imu.h"
-//1  600 1800  1300
-//2  750 1550  1250
-//3 1300 2000 1700
-//4 1100 1700 1350
-//5  800 2000 
-//6  800 2000
-//7           2500
-//8           800
-//9  1200 1700
-//10 1500 2000
-//11 850  1350
-//12 1300 1800
-int STEER[12] = {1300,1200,1700,1375,1700,1200,1800,1500,1700,1500,1300,1350};
+#include "legctrl.h"
+#include "remote.h"
 void StartMainTask(void const * argument)
 {
 	for(;;)
 	{
 		IMU_UPDATE();
-//				__HAL_TIM_SET_COMPARE(&htim3,TIM_CHANNEL_2,a);
-//			HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_2);
-//		__HAL_TIM_SET_COMPARE(&htim5,TIM_CHANNEL_3,STEER[0]);
-//			HAL_TIM_PWM_Start(&htim5,TIM_CHANNEL_3);
-//		__HAL_TIM_SET_COMPARE(&htim8,TIM_CHANNEL_3,STEER[1]);
-//			HAL_TIM_PWM_Start(&htim8,TIM_CHANNEL_3);
-//		__HAL_TIM_SET_COMPARE(&htim4,TIM_CHANNEL_2,STEER[2]);
-//			HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_2);
-//		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,STEER[3]);
-//			HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_1);
-//		__HAL_TIM_SET_COMPARE(&htim5,TIM_CHANNEL_4,STEER[4]);
-//			HAL_TIM_PWM_Start(&htim5,TIM_CHANNEL_4);
-//		__HAL_TIM_SET_COMPARE(&htim8,TIM_CHANNEL_4,STEER[5]);
-//			HAL_TIM_PWM_Start(&htim8,TIM_CHANNEL_4);
-//		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,STEER[6]);
-//			HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_2);
-//		__HAL_TIM_SET_COMPARE(&htim4,TIM_CHANNEL_1,STEER[7]);
-//			HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1);
-//		__HAL_TIM_SET_COMPARE(&htim4,TIM_CHANNEL_3,STEER[8]-RC_Ctl.rc0.ch0);
-//			HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_3);
-//		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_3,STEER[9]+RC_Ctl.rc0.ch0);
-//			HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_3);
-//		__HAL_TIM_SET_COMPARE(&htim5,TIM_CHANNEL_2,STEER[10]-RC_Ctl.rc0.ch0);
-//			HAL_TIM_PWM_Start(&htim5,TIM_CHANNEL_2);
-//			__HAL_TIM_SET_COMPARE(&htim8,TIM_CHANNEL_2,STEER[11]+RC_Ctl.rc0.ch0);
-//			HAL_TIM_PWM_Start(&htim8,TIM_CHANNEL_2);
+		Remote_Handle();
 		osDelay(1);
 	}
 }
 
 void StartShowTask(void const * argument)
 {
-//	__HAL_TIM_SET_AUTORELOAD(&htim4,38000);
-//	__HAL_TIM_SET_COMPARE(&htim4,TIM_CHANNEL_4,(38000/2+1));
+	for(;;)
+	{
+
+		osDelay(50);
+	}
+
 }
